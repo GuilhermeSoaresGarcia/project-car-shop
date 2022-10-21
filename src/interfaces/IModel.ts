4351d9ebc4008e65bdf255d6ccf2ec8c7ecc41c0
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-21 17:25:15 -0300

    refactor: Alterados nomes em alguns parâmetros dos métodos declarados para ficar mais descritivo

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,7 +1,7 @@
 export interface IModel<T> {
   create(obj: T): Promise<T>;
   read(): Promise<T[]>;
-  readOne(value: string): Promise<T | null>;
-  update(value: string, obj: T): Promise<T | null>;
-  delete(value: string): Promise<T | null>;
+  readOne(_id: string): Promise<T | null>;
+  update(_id: string, obj: T): Promise<T | null>;
+  delete(_id: string): Promise<T | null>;
 }
