48d8a53b16502bdb4d043ff4ea7c5687ee5cbb17
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-22 19:09:21 -0300

    feat: importados no app.ts rota para 'cars' e middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carRoutes from './routes/car';
 
 const app = express();
 app.use(express.json());
+app.use(carRoutes);
+app.use(errorHandler);
 
 export default app;
