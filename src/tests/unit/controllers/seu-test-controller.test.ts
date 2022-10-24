1bde75640f0588440089af89ef5787f81fb35692
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-24 18:30:47 -0300

    test: testes camada controller

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -1,22 +1,66 @@
-// template para criação dos testes de cobertura da camada de controller
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { NextFunction, Request, Response } from 'express';
+import { carMock, carMockWithId } from '../../mocks/carMocks';
+import CarController from '../../../controllers/CarControllers';
+import CarService from '../../../services/CarServices';
+import CarModel from '../../../models/CarModel';
 
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+describe('Frame Controller', () => {
+  const carModel = new CarModel()
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+  // fazemos o cast de um objeto para um `Request` pois nosso controller só vai aceitar um objeto do tipo `Request` como primeiro parâmetro
+  const req = {} as Request;
+  // o mesmo acontece com o segundo parâmetro
+  const res = {} as Response;
 
-// describe('Sua descrição', () => {
+  before(() => {
+    sinon.stub(carService, 'create').resolves(carMock);
+    sinon.stub(carService, 'readOne').resolves(carMock);
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  after(() => {
+    sinon.restore()
+  })
 
-//   it('', async () => {});
+  describe('método create', () => {
+    it('happy route', async () => {
+      req.body = carMock;
+      await carController.create(req, res);
+      // o cast de `res.status` é feito pois `res` foi criado como do tipo `Request` 
+      // e agora, que queremos validar com o que foi chamado, precisar ser tratado como um `sinon.SinonStub`
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+  });
 
-// });
+  describe('método readOne', () => {
+    it('happy route', async () => {
+      // como fizemos o dublê da service o valor do `req.params.id` não vai chegar na model
+      // logo ele só precisa ser um string e existir
+      req.params = { id: carMockWithId._id };
+      await carController.readOne(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+
+    it('error', async () => {
+      // let error;
+      // try {
+        req.params = { id: '1234567890' };
+        await carController.readOne(req, res);
+      // } catch (err: any) {
+      //   err.message = 'Id must have 24 hexadecimal characters'
+      // }
+
+      expect((res.status as sinon.SinonStub).calledWith(400)).to.be.true;
+      // expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+  });
+});
