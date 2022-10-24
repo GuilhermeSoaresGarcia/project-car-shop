c53f0bcd8b086d1dbf45312b6b99b6b216967f21
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-24 17:17:57 -0300

    refactor: corrigida a tipagem do objeto no método update

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,4 +1,4 @@
-import { isValidObjectId, Model } from 'mongoose';
+import { isValidObjectId, Model, UpdateQuery } from 'mongoose';
 import { IModel } from '../interfaces/IModel';
 import { ErrorTypes } from '../errors/catalog';
 
@@ -22,9 +22,9 @@ export default abstract class MongoModel<T> implements IModel<T> {
     return this._model.findOne({ _id });
   }
 
-  public async update(_id: string, obj: any): Promise<T | null> { // como utilizar o tipo correto?
+  public async update(_id: string, obj: T): Promise<T | null> {
     if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
-    return this._model.findByIdAndUpdate({ _id }, { ...obj }) as unknown as T;   
+    return this._model.findByIdAndUpdate({ _id }, { ...obj as UpdateQuery<T> }) as unknown as T;
   }
 
   public async delete(_id: string): Promise<T | null> {
