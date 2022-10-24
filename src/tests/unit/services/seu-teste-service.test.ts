63c76f8f8b858e7b1898568f7846884aedb01f26
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-24 18:30:34 -0300

    test: testes camada service

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,22 +1,60 @@
-// template para criação dos testes de cobertura da camada de service
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../services/CarServices';
+import { carMock, carMockWithId } from '../../mocks/carMocks';
 
+describe('Car Service', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+  before(() => {
+    sinon.stub(carModel, 'create').resolves(carMockWithId);
+    sinon.stub(carModel, 'readOne')
+      // na chamada de index 0 `carModel.readOne` vai responder um fakeCar
+      .onCall(0).resolves(carMockWithId)
+      // já na próxima chamada ele vai mudar seu retorno, isso pode ser feito várias vezes
+      .onCall(1).resolves(null);
+  })
+  after(() => {
+    sinon.restore()
+  })
 
-// describe('Sua descrição', () => {
+  describe('método create', () => {
+    it('happy route', async () => {
+      const carCreated = await carService.create(carMock);
+      expect(carCreated).to.be.deep.equal(carMockWithId);
+    });
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+    it('error', async () => {
+      let error;
+      try {
+        await carService.create({});
+      } catch (err) {
+        error = err
+      }
+      expect(error).to.be.instanceOf(ZodError);
+    });
+  });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  describe('método readOne', () => {
+    it('happy route', async () => {
+      const carCreated = await carService.readOne(carMockWithId._id);
+      expect(carCreated).to.be.deep.equal(carMockWithId);
+    });
 
-//   it('', async () => {});
-
-// });
+    it('error', async () => {
+      let error;
+      try {
+        // a mesma chamada que o teste acima aqui vai gerar o erro por causa do nosso sinon.stub(...).onCall(1)
+        await carService.readOne(carMockWithId._id);
+      } catch (err: any) {
+        error = err
+      }
+      expect(error, 'error should be defined').not.to.be.undefined;
+      expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+    });
+  });
+});
