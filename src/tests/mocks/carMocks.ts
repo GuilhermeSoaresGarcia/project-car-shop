7a0c3588177b10ee5466013016aa6fe01581676c
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-24 19:59:01 -0300

    test: implementado mock para o retorno do read

--- src/tests/mocks/carMocks.ts
+++ src/tests/mocks/carMocks.ts
@@ -1,15 +1,42 @@
 import { ICar } from "../../interfaces/ICar";
 
-const carMock:ICar = {
+const carMock: ICar = {
   model: 'Cabritinha',
   year: 2005,
   color: 'Dark blue',
   buyValue: 12000,
   doorsQty: 4,
   seatsQty: 3,
-}  
+}
 
-const carMockWithId:ICar & { _id:string } = {
+const allCarsMock: ICar[] = [
+  {
+  model: 'Cabritinha',
+  year: 2005,
+  color: 'Dark blue',
+  buyValue: 12000,
+  doorsQty: 4,
+  seatsQty: 3,
+},
+{
+  model: 'Versa do Dante',
+  year: 2013,
+  color: 'Black',
+  buyValue: 27000,
+  doorsQty: 6,
+  seatsQty: 3,
+},
+{
+  model: 'Siena do pai',
+  year: 2006,
+  color: 'Silver',
+  buyValue: 14000,
+  doorsQty: 6,
+  seatsQty: 3,
+},
+]
+
+const carMockWithId: ICar & { _id: string } = {
   _id: '62cf1fc6498565d94eba52cd',
   model: 'Cabritinha',
   year: 2005,
@@ -19,4 +46,4 @@ const carMockWithId:ICar & { _id:string } = {
   seatsQty: 3,
 };
 
-export { carMock, carMockWithId };
+export { carMock, allCarsMock, carMockWithId };
