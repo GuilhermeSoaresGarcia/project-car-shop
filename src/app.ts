492935562adf7bee7957cb274e15a1ae7b5cdedc
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-25 16:23:12 -0300

    feat: importada no app.ts rota para 'motorcycle'

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
 import carRoutes from './routes/CarsRoutes';
+import motorcycleRoutes from './routes/MotorcyclesRoutes';
 
 const app = express();
 app.use(express.json());
 app.use(carRoutes);
+app.use(motorcycleRoutes);
 app.use(errorHandler);
 
 export default app;
